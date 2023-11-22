4911ed1828372749d986309deab0a5f0565f2a3b
YanaKh <yanahomiak99@gmail.com>
2023-11-23 00:09:10 +0200

    fix next.config

--- next.config.js
+++ next.config.js
@@ -8,6 +8,9 @@ const nextConfig = { images: {
                 pathname: '/**',
             },
         ],
+    },
+    experimental: {
+        serverActions: true,
     },}
 
 module.exports = nextConfig
